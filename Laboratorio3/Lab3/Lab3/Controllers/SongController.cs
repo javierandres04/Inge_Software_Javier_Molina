ac7e21529691a803c7c0592334738ca67eaaba73
Javier Molina Herrera <javmoli045@gmail.com>
2022-04-28 17:25:22 -0600

    Agregado el archivo SongController.cs

---
+++ Laboratorio3/Lab3/Lab3/Controllers/SongController.cs
@@ -0,0 +1,21 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            SongModel myFavoriteSong = new SongModel
+            {
+                Nombre = "Don't Stop Me Now",
+                Artista = "Queen",
+                Album = "Jazz",
+                Genero = "pop rock, piano rock",
+                FechaEstreno = new DateTime(1979, 01, 05)
+            };
+            return View(myFavoriteSong);
+        }
+    }
+}
