a0bfeca267022dd5fae61de4a3f8f2be28afdcdc
Javier Molina Herrera <javmoli045@gmail.com>
2022-04-28 17:23:02 -0600

    Agregado el modelo SongModel.cs

---
+++ Laboratorio3/Lab3/Lab3/Models/SongModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab3.Models
+{
+    public class SongModel
+    {
+        public String Nombre { get; set; }
+        public String Artista { get; set; }
+        public String Album { get; set; }
+        public String Genero { get; set; }
+        public DateTime FechaEstreno { get; set; }
+    }
+}
