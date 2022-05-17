48585d8f930b4cc9cb268d698015571265c48bf8
Javier Molina Herrera <javmoli045@gmail.com>
2022-05-17 12:59:49 -0600

    Agregado el laboratorio 5

---
+++ Laboratorio5/Laboratorio5/Laboratorio5/Controllers/PaisesController.cs
@@ -0,0 +1,16 @@
+﻿using Laboratorio5.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorio5.Controllers
+{
+    public class PaisesController : Controller
+    {
+        public IActionResult Index()
+        {
+            PaisesHandler paisesHandler = new PaisesHandler();
+            var paises = paisesHandler.ObtenerPaises();
+            ViewBag.MainTitle = "Lista de países";
+            return View(paises); ;
+        }
+    }
+}
