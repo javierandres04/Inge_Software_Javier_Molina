cfe25b54e747d73e79bf1b5749c0ed9b140a1dbe
Javier Molina Herrera <javmoli045@gmail.com>
2022-06-29 18:13:43 -0600

    Agregado test para lab 8

---
+++ Laboratorio5/Laboratorio5/TestProjectLaboratorio8/Selenium.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace TestProjectLaboratorio8
+{
+    public class Selenium
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void PruebaIngresoCrearPaises()
+        {
+            string URL = "";
+            driver.Manage().Window.Maximize();
+            driver.Url = URL;
+            Assert.IsNotNull(driver);
+        }
+    }
+
+}
